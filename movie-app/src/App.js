7eb59833cb7d342cd4d092353ec109201bfa510a
MarwenCode <marwen_jdidi@yahoo.com>
2022-10-03 16:54:30 +0200

    set app

---
+++ movie-app/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import MovieDetails from "./components/movieDetails/MovieDetails";
+import PageNotFound from "./components/pageNotFound/PageNotFound";
+import "./App.scss";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/pageNotFound" element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
